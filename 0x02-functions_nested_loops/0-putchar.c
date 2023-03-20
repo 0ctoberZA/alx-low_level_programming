923c285d05b944b58dc466a63277bf17802731dc
0ctoberZA <tsakanilindsay@gmail.com>
2023-03-20 05:56:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <header.h>
+/**
+ * main - program that prints _putchar.
+ *
+ * Return: always 0 (success)
+ */
+int main()
+
+	for (
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <functions>
+/**
+ * main - prints the alphabet, in lowercase
+ *
+ * Return:always 0 (success)
+ */
+
+void print_alphabet(void);
+
+char letter = 'a';
+
+while (letter <= 'z')
+{
+	_putchar(letter);
+	letter++;
+}
+_putchar('\n');
+
+}
