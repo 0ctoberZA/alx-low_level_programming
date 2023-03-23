cde69ee3412014a00855f9ecfee9e98aa7cbd8e6
0ctoberZA <tsakanilindsay@gmail.com>
2023-03-23 11:43:38 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,16 @@
  * print_most_numbers - prints the numbers, from 0 to 9
  * Do not print 2 and 4
  *
- * Return: always 0
  */
 void print_most_numbers(void)
 {
-	int num;
+	char i;
 
-	for (num = 0; num <= 9; num++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (num != 2 && num != 4)
-		_putchar((num % 10) + '0');
+		if (i != '2' && i != '4')
+		_putchar(i);
 	}
 
-	_putchar('\n')
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,5 +1,4 @@
-#include <unistd.h>
-#include "main.h"
+#include <unustd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
