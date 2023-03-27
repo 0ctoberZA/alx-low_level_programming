388769453998bce882f8584f09b38b39111de96f
0ctoberZA <tsakanilindsay@gmail.com>
2023-03-27 05:01:52 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * 0-reset_to_98 - takes a pointer to an int as parameter and updates its value
+ * reset_to_98 - takes a pointer to an int as parameter and updates its value
  * @n: input integer
  *
  */
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 
 void reset_to_98(int *n);
-
+int _strlen(char *s);
 
 #endif
