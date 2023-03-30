cafd2c591c26cefbaf84b10b053bfcc8c2889bdc
0ctoberZA <tsakanilindsay@gmail.com>
2023-03-30 01:56:49 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if s([i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+		while ([i] != '\0')
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z') 
+					&& (s[i - 1] == ' ' ||
+						s[i - 1] == '\t' || s[i - 1] == '\n' ||
+						s[i - 1] == ',' || s[i - 1] == ';' ||
+						s[i- 1] == '.' || s[i - 1] == '!' ||
+						s[i - 1] == '?' || s[i - 1] == '"' ||
+						s[i - 1] == '(' || s[i - 1] == ')' ||
+						s[i - 1] == '{' || s[i - 1] == '}' ||))
+						s[i] = s[i] - 'a' + 'A';
+			i++;
+	} 
+	return (s);
+}
