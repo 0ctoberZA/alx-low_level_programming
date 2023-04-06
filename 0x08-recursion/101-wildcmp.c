7beef427cef1fe5dbdc4e04543e119c8136efdc0
0ctoberZA <tsakanilindsay@gmail.com>
2023-04-06 05:36:36 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == '\0' && *s2 == '*')
-		return (subtring_match(s1, s2 + 1, s2 + 1));
+		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
 	if (*s2 == '*')
@@ -19,7 +19,7 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	if (*s1 == *s2)
 		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
 	else
-		return (substring_match(s1 + 1, after_wildcd, after_wldcd));
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
 }
 /**
  * wildcmp - compare if string with wildcard matches
