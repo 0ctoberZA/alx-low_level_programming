a8ace502363cc370518cbf8fc8c668a6b336bae3
0ctoberZA <tsakanilindsay@gmail.com>
2023-04-28 06:50:28 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,12 +5,12 @@
  * @head: head of the linked list
  * @str: string to store the list
  *
- * Return:  address of the new element, or NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t tchar;
+	size_t tchar;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
