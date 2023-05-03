5d0f6a6ba8c4ba1896b4dff4f611aab278e90944
0ctoberZA <tsakanilindsay@gmail.com>
2023-05-03 12:25:03 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,2 +1,7 @@
 #include "lists.h"
-
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: head of linked list
+ *
+ * 
+ */
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * find_listint_loop -  finds the loop in a linked list.
+ * @head: head of linked list
+ *
+ * Return: address of node where loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *cat, *mouse;
+
+	cat = mouse = head;
+
+	while (cat != NULL && hare != NULL)
+	{
+		cat = cat->next;
+		mouse = mouse->next->next;
+
+		if (cat == mouse)
+		{
+			cat = mouse;
+			while (cat != mouse)
+			{
+				cat = cat->next;
+				mouse = mouse->next;
+			}
+			return (cat);
+		}
+	}
+	return (NULL);
+}
