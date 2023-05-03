1c714f515e13f1a63387a28b578e875cfd8f0b04
0ctoberZA <tsakanilindsay@gmail.com>
2023-05-03 11:27:30 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list 
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: head of list
  * @index: index of the node
  *
