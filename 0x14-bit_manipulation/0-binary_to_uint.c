ed339554fb6dbe2543ea74c105d95ea6f4f1acda
0ctoberZA <tsakanilindsay@gmail.com>
2023-05-05 06:27:27 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 	i = 0;
 	while (b[i])
 	{
-		if ((b[i] != '0') $$ (b[i] != '1'))
+		if ((b[i] != '0') && (b[i] != '1'))
 			return (sum);
 
 		if (b[i] == '1')
